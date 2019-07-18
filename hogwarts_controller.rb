48c57b26d72ae1369b1ba79d07e5f56b5ecddce0
= <evebramley@me.com>
2019-07-18 13:09:31 +0100

    initialized sinatra

---
+++ hogwarts_controller.rb
@@ -0,0 +1,18 @@
+require ( 'sinatra' )
+require ( 'sinatra/contrib/all' )
+require ( 'pry-byebug' )
+
+require_relative( './models/student.rb' )
+also_reload( './models/*' )
+
+#INDEX
+get '/students' do
+  @students = Student.all()
+  erb( :index )
+end
+
+#SHOW
+get '/students/:id' do
+  @student = Student.find( params[:id] )
+  erb( :show )
+end
--- models/student.rb
+++ models/student.rb
@@ -20,15 +20,35 @@ class Student
       last_name,
       house,
       age
-      )
-      VALUES
-      (
-        $1, $2, $3, $4
-        )
-        RETURNING *"
-        values = [@first_name, @last_name, @house, @age]
-        student_data = SqlRunner.run(sql, values)
-        @id = student_data.first()['id'].to_i
+    )
+    VALUES
+    (
+      $1, $2, $3, $4
+    )
+    RETURNING *"
+    values = [@first_name, @last_name, @house, @age]
+    student_data = SqlRunner.run(sql, values)
+    @id = student_data.first()['id'].to_i
+  end
+
+      def self.all()
+        sql = "SELECT * FROM students"
+        students = SqlRunner.run( sql )
+        result = students.map { |student| Student.new(student) }
+        return result
       end
+
+      def self.find( id )
+        sql = "SELECT * FROM students
+        WHERE id = $1"
+        values = [id]
+        student = SqlRunner.run( sql, values )
+        result = Student.new( student.first )
+        return result
+      end
+
+
+
+
 
 end
